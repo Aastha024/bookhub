3331a6b656d285d21c9dd8332875f1ddc75eb1af
Aastha024 <aastha.shah@seaflux.tech>
2025-04-04 23:18:53 +0530

    feat: initialize project with Express, TypeScript, and MongoDB setup add linting and ignore files

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import bodyParser from "body-parser";
+import authRoute from "./modules/auth/auth.route";
+
+dotenv.config();
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, MongoDB with TypeScript!");
+});
+app.use("/api/auth", authRoute); 
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+// TRequest extends the Express Request to include a 'dto' property for validated data.
+export interface TRequest<T> extends Request {
+  dto: T;
+}
+
+// TResponse is an alias for Express Response.
+export type TResponse = Response;
