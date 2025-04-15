8282487ef648d230ee02e86171d70eaa34046aad
Aastha024 <aastha.shah@seaflux.tech>
2025-04-15 19:54:30 +0530

    refactor: update the route & add category route

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 import connectDB from "./config/db";
 import bodyParser from "body-parser";
 import authRoute from "./modules/auth/auth.route";
-import bookRoute from "./modules/booklist/book.route";
+import bookRoute from "./modules/book/book.route";
 import roleRoute from "./admin/modules/role/role.route"
+import categoryRoute from "./admin/modules/category/category.route";
 import 'module-alias/register';
 
 dotenv.config();
@@ -22,8 +23,9 @@ app.get("/", (req, res) => {
   res.send("Hello, MongoDB with TypeScript!");
 });
 app.use("/api/auth", authRoute); 
-app.use("/admin", bookRoute)
+app.use("/book", bookRoute)
 app.use("/admin", roleRoute)
+app.use("/admin/category", categoryRoute)
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
